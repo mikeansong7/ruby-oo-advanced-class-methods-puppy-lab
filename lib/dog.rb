9dd72a940d09a295ddd3c0ba64f827244cd42ce0
github username <github email address>
2021-01-17 10:52:47 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+class Dog
+    attr_reader :dog
+    @@all = []
+
+    def initialize(dog)
+        @dog = dog
+        @@all = save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+       puts @@all.map{|dog| dog.dog}
+    end
+
+    def save
+        @@all << self
+    end 
+
+    def name
+        @dog
+    end
+
+end
+
+
+
+
+
+
